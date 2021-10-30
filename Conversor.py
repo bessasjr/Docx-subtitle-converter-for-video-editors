9b15b097402561b0311c90bde33871ad0af49ef0
Junior Bessas <93383793+bessasjr@users.noreply.github.com>
2021-10-30 12:35:26 -0300

    Update Conversor.py

--- Conversor.py
+++ Conversor.py
@@ -4,7 +4,7 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-leg = docx2txt.process('03.docx').splitlines()
+leg = docx2txt.process('<insert your subtitle file>.docx').splitlines()
 leg = leg[0::2]
 
 cont = 0
@@ -14,10 +14,10 @@ for line in leg:
     MAX_W, MAX_H = 1920, 1080
     img = Image.new('RGBA', (MAX_W, MAX_H), (0, 0, 0, 0))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('arcon.regular.otf', 50)
+    font = ImageFont.truetype('enter a text font for the caption', 50) # 50 is the font size
     current_h, pad = 960, 20
     textwrap.wrap(line, width=100)
     w, h = draw.textsize(line, font=font)
     draw.text((((MAX_W - w) / 2), current_h), line, font=font)
     current_h += h + pad
-    img.save('Legendas/{}.png'.format(cont))
+    img.save('<choose the folder to save the files>/{}.png'.format(cont))
